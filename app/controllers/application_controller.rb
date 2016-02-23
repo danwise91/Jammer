aa8f756657daeed161fe81bae9d997583a223dd7
Dan Wise <dwise2@oswego.edu>
2016-02-23 14:26:07 -0500

    fixed one more issue with login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,4 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
   include SessionsHelper
 
-  # private
-  # def current_user
-  #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  #    end
-  #   helper_method :current_user
-  # end
-  #
-  # def logged_in?
-  #   !!current_user
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,24 +6,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    #integerating both soundcloud signin and also MM signin
-    if params[:provider]
-      user = User.from_omniauth(env["omniauth.auth"])
-      session[:id] = user.id
-      redirect_to user
-    else
     @user = User.find_by(email: params[:user][:email])
     if @user && @user.authenticate(params[:user][:password])
       log_in @user
       redirect_to @user
-  end
-    # render 'new'
-    # @user = User.new(params[:email], params[:password])
-    #  user = User.find_by(params[:email], params[:password])
-    # if user && user.authenticate(params[:password])
-    #   # session[:user_id]= user.id
-    #   redirect_to root_path
-    # else
+  else
       flash[:danger] = "Bad username or password"
       render :new
     end
